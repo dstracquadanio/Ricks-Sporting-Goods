80711ec983cbeaadf488799e7afb9999f7777ed4
Cameron Ratliff <cratliff@colgate.edu>
2020-04-26 15:23:30 -0400

    submitpage

--- client/components/submitPage.js
+++ client/components/submitPage.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {Component} from 'react'
 import CheckCircleTwoToneIcon from '@material-ui/icons/CheckCircle'
 
-const submitPage = () => {
-  return (
-    <div className="container-12">
-      <CheckCircleTwoToneIcon id="order-placed-icon" />
-      <div>Thank you for your purchase!</div>
-    </div>
-  )
+class submitPage extends Component {
+  constructor() {
+    super()
+  }
+  componentDidMount() {
+    console.log('hi')
+  }
+
+  render() {
+    console.log('hi2')
+    return (
+      <div className="container-12">
+        <CheckCircleTwoToneIcon id="order-placed-icon" />
+        <div>Thank you for your purchase!</div>
+      </div>
+    )
+  }
 }
 
 export default submitPage
